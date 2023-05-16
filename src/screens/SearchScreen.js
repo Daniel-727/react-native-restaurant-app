7b5513066743d27d3bde2134bb17db896db5f91a
Daniel Li <zaoster777@gmail.com>
2023-05-16 15:53:43 -0500

    added ResultsList component and separated results into three price groups

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,16 +2,27 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   // Destructuring our custom hook
   const [results, error, searchApi] = useResults();
 
+  const filterResultsByPrice = (price) => {
+    // Price === '$' || '$$' || '$$$'
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
+
   return (
     <View style={styles.page}>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
       {error ? <Text>{error}</Text> : <Text>We have found {results.length} results</Text>}
+      <ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
     </View>
   );
 };
